b31e136f40350b28adddafb882520cf7e33964d6
shah1dss <you@example.com>
2022-05-22 17:10:21 +0300

    add game board and classes of figures

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './modules/Board';
+
+function App() {
+  const [board, setBoard] = useState(new Board());
+
+useEffect(() =>{
+  restart()
+},[])
+
+  function restart(){
+    const newBoard = new Board();
+    newBoard.initCells();
+    newBoard.addFigurs();
+    setBoard(newBoard);
+  }
+  return (
+    <div className="app">
+      <BoardComponent
+      board={board}
+      setBoard={setBoard}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { Cell } from '../modules/Cell';
+
+interface CellProps {
+  cell: Cell;
+}
+
+const CellComponent: FC<CellProps> = ({cell}) => {
+  return (
+    <div 
+    className={['cell', cell.color].join(' ')}
+    >
+      {cell.figure?.logo && <img src={cell.figure.logo} alt={cell.figure.name}/>}
+    </div>
+  );
+};
+
+export default CellComponent;
